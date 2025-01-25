dc4d6d46f5f2a9f88e0c47f9d285b43531ae70a7
jezzer218 <josh.h218@gmail.com>
2025-01-25 12:44:58 -0600

    First commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import *
+
+
+def main():
+    dummy_node = TextNode("This is a text node", TextType.BOLD_TEXT, "https://www.boot.dev")
+    print(dummy_node)
+
+
+
+if __name__ == "__main__":
+    main()
