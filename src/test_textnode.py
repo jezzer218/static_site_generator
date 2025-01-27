564225a25fbdbf43b0742fece34f4fd1349b690c
jezzer218 <josh.h218@gmail.com>
2025-01-26 18:02:13 -0600

    2nd commit

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,20 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD_TEXT)
         node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
         self.assertEqual(node, node2)
+        
+    def test_text_type(self):
+        t_node = TextNode("This is a text node", TextType.BOLD_TEXT)
+        t_node2 = TextNode("This is a text node", TextType.BOLD_TEXT)
+        self.assertEqual(t_node.text_type, t_node2.text_type)
+        
+    def test_url_defualt(self):
+        t_node = TextNode("This is a text node", TextType.BOLD_TEXT,)
+        self.assertTrue(t_node.url == None)
+        
+    def test_url_set(self):
+        url = "https://www.boot.dev"
+        node = TextNode("This is a text node", TextType.BOLD_TEXT, url)
+        self.assertEqual(node.url, url)
 
 
 if __name__ == "__main__":
